3fb72872d6faba26e8c71203e1fa5cacb15915b2
hisham waleed karam <hisham.karam@cartologic.com>
2018-11-25 16:17:28 +0200

    fix cartoview1.10.x

--- urls.py
+++ urls.py
@@ -1,17 +1,16 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 # Seperate Attachments API
 from .views import AttachmentApi, index
 
 api = AttachmentApi()
-urlpatterns = patterns('',
-                       url(r'^$', index),
-                       url(r'^(?P<layername>\w+)/(?P<attachment_type>comment|file)$',
-                           api.attachments_list_create, name="attachment_list"),
-                       url(r'^(?P<layername>\w+)/(?P<attachment_type>comment|file)/(?P<id>\d+)$',
-                           api.attachments_details_update, name="attachment_details"),
-                       url(r'^(?P<layername>\w+)/file/(?P<id>\d+)/download$',
-                           api.attachments_download, name="attachment_download"),
-                       url(r'^tags$',
-                           api.tags_list, name="tags_list"),
-                       )
+urlpatterns = [
+    url(r'^$', index),
+    url(r'^(?P<layername>\w+)/(?P<attachment_type>comment|file)$',
+        api.attachments_list_create, name="attachment_list"),
+    url(r'^(?P<layername>\w+)/(?P<attachment_type>comment|file)/(?P<id>\d+)$',
+        api.attachments_details_update, name="attachment_details"),
+    url(r'^(?P<layername>\w+)/file/(?P<id>\d+)/download$',
+        api.attachments_download, name="attachment_download"),
+    url(r'^tags$', api.tags_list, name="tags_list"),
+]
