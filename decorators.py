cbb7cceca1aef9db82b43363bc0c0d36a914dcc5
khaled <bokae97@gmail.com>
2020-12-21 13:30:36 +0200

    fixed: bool obj is not callable

--- decorators.py
+++ decorators.py
@@ -8,7 +8,7 @@ def methods_permission(view):
     @wraps(view)
     def check(request, *args, **kwargs):
         method = request.method
-        if request.user.is_authenticated() or method == "GET":
+        if request.user.is_authenticated or method == "GET":
             return view(request, *args, **kwargs)
         elif not request.user.is_authenticated() and method != "GET":
             return HttpResponse(json.dumps({"error": "Unauthorized"}),
